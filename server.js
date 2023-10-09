10bfa93cac22e865790154d06298fefbf4777dc2
h-bura <hsyn.bura@gmail.com>
2023-10-09 10:33:46 +0300

    the indices of the two numbers targeted

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const func = require("./algorithms/countZero");
+const func = require("./algorithms/twoSum");
 
-const result = func.countZero(50);
+const result = func.twoSum([2, 7, 11, 15], 9);
 console.log({ result });
 const port = 3000;
 app.listen(port, () => {
